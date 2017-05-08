25771e423ad5f47c644598ded16b7d40b5093ab5
Valerie Trudell <v.trudell@protonmail.com>
2017-05-08 14:56:13 -0600

    adds seeds for seed table

--- db/seeds/dev/foods.js
+++ db/seeds/dev/foods.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
-  //return knex.raw('TRUNCATE foods RESTART IDENTITY')
-  return knex('foods').del()
+  return knex.raw('TRUNCATE foods RESTART IDENTITY')
+  //return knex('foods').del()
   .then(() => {
     return Promise.all ([
       knex.raw (
@@ -9,39 +9,39 @@ exports.seed = function(knex, Promise) {
         ["French Silk Pie", 340]
       ),
       knex.raw (
-        'INSERT INTO foods (food_name, calories) VALUES (?, ?)'
+        'INSERT INTO foods (food_name, calories) VALUES (?, ?)',
         ["Orange", 34]
       ),
 
       knex.raw (
-        'INSERT INTO foods (food_name, calories) VALUES (?, ?)'
+        'INSERT INTO foods (food_name, calories) VALUES (?, ?)',
         ["Deep Dish Pizza", 890]
       ),
 
       knex.raw (
-        'INSERT INTO foods (food_name, calories) VALUES (?, ?)'
+        'INSERT INTO foods (food_name, calories) VALUES (?, ?)',
         ["Spinach Salad w/ dressing", 240]
       ),
 
       knex.raw (
-        'INSERT INTO foods (food_name, calories) VALUES (?, ?)'
+        'INSERT INTO foods (food_name, calories) VALUES (?, ?)',
         ["Roasted Cauliflower", 80]
       ),
 
       knex.raw (
-        'INSERT INTO foods (food_name, calories) VALUES (?, ?)'
+        'INSERT INTO foods (food_name, calories) VALUES (?, ?)',
         ["Chicken Breast", 210]
       ),
 
       knex.raw (
-        'INSERT INTO foods (food_name, calories) VALUES (?, ?)'
+        'INSERT INTO foods (food_name, calories) VALUES (?, ?)',
         ["Dark Chocolate", 150]
       ),
 
       knex.raw (
-        'INSERT INTO foods (food_name, calories) VALUES (?, ?)'
+        'INSERT INTO foods (food_name, calories) VALUES (?, ?)',
         ["Beef Jerky", 95]
-      ),
+      )
 
     ]);
   });
