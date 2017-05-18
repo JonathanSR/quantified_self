3032e12771a1c5a8ad8f490a944cb6e95439b64a
Valerie Trudell <v.trudell@protonmail.com>
2017-05-18 00:59:55 -0600

    takes out migration and adds production folder

---
+++ db/seeds/dev/foods.js
@@ -0,0 +1,50 @@
+
+exports.seed = function(knex, Promise) {
+  return knex.raw('TRUNCATE foods RESTART IDENTITY')
+  .then(() => {
+    return Promise.all ([
+      knex.raw (
+        'INSERT INTO foods (food_name, calories) VALUES (?, ?)',
+        ["Banana", 34]
+      ),
+      knex.raw (
+        'INSERT INTO foods (food_name, calories) VALUES (?, ?)',
+        ["French Silk Pie", 340]
+      ),
+      knex.raw (
+        'INSERT INTO foods (food_name, calories) VALUES (?, ?)',
+        ["Orange", 34]
+      ),
+
+      knex.raw (
+        'INSERT INTO foods (food_name, calories) VALUES (?, ?)',
+        ["Deep Dish Pizza", 890]
+      ),
+
+      knex.raw (
+        'INSERT INTO foods (food_name, calories) VALUES (?, ?)',
+        ["Spinach Salad w/ dressing", 240]
+      ),
+
+      knex.raw (
+        'INSERT INTO foods (food_name, calories) VALUES (?, ?)',
+        ["Roasted Cauliflower", 80]
+      ),
+
+      knex.raw (
+        'INSERT INTO foods (food_name, calories) VALUES (?, ?)',
+        ["Chicken Breast", 210]
+      ),
+
+      knex.raw (
+        'INSERT INTO foods (food_name, calories) VALUES (?, ?)',
+        ["Dark Chocolate", 150]
+      ),
+
+      knex.raw (
+        'INSERT INTO foods (food_name, calories) VALUES (?, ?)',
+        ["Beef Jerky", 95]
+      )
+    ]);
+  });
+};
---
+++ db/seeds/dev/meals.js
@@ -0,0 +1,21 @@
+exports.seed = function(knex, Promise) {
+  return knex('meals').then(() => {
+    return Promise.all ([
+      knex.raw (
+        'INSERT INTO meals (meal_name) VALUES (?)',
+        ["Breakfast"]),
+      knex.raw (
+        'INSERT INTO meals (meal_name) VALUES (?)',
+        ["Lunch"]
+      ),
+      knex.raw (
+        'INSERT INTO meals (meal_name) VALUES (?)',
+        ["Dinner"]
+      ),
+      knex.raw (
+        'INSERT INTO meals (meal_name) VALUES (?)',
+        ["Snack"]
+      )
+    ]);
+  });
+};
